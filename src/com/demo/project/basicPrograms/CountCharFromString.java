e17b7c4c27f0a92d7f361720d5c1f0af159c6e20
pankajkrravi <pankajravi25@gmail.com>
2021-09-13 09:00:13 +0530

    Count char from Str

---
+++ src/com/demo/project/basicPrograms/CountCharFromString.java
@@ -0,0 +1,37 @@
+package com.demo.project.basicPrograms;
+
+/**
+ * @Author pankaj
+ * @create 8/23/21 1:33 PM
+ */
+import java.util.*;
+
+ /*input         String Str = "JAVA";
+ output;
+ j = 1
+a= 2
+v = 1
+*/
+public class  CountCharFromString {
+
+    public static void main(String[] args) {
+
+        String Str = "JAVA";
+
+        char charStr[] = Str.toCharArray();
+
+        int count = 0;
+        for (int i = 0; i < charStr.length; i++) {
+
+            for (int j = 0; j < charStr.length; j++) {
+
+                if (charStr[i] == charStr[j]) {
+                    count++;
+
+                } if(count ==1 ) {
+                    System.out.println(charStr[i] + " = " + count);
+                }
+            }
+        }
+    }
+    }
